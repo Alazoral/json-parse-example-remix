9fe06881e28583cdee81c6b22339189424fd0765
Leon Spencer <682772+Alazoral@users.noreply.github.com>
2022-02-01 12:11:26 +1100

    Add fs version

--- app/routes/crash.js
+++ app/routes/crash.js
@@ -1,7 +1,14 @@
 import {json} from "remix";
+import fs from 'fs/promises'
+import {join} from "path";
 
+export const loader = async () => {
+    const loadedText = await fs.readFile(join(process.cwd(), 'data', 'crash.json'), 'utf8')
+    const requestJson = JSON.parse(loadedText)
+    return json({status: 'All Good from FS', count: requestJson.data.length})
+}
 export const action = async ({request, params}) => {
     const requestText = await request.text()
     const requestJson = JSON.parse(requestText)
-    return json({ status: 'All Good', count: requestJson.data.length })
+    return json({status: 'All Good from Body', count: requestJson.data.length})
 }
