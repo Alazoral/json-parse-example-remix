e25d90e91daa19dcaee913fed0ba992010a777df
Leon Spencer <682772+Alazoral@users.noreply.github.com>
2022-02-01 12:45:41 +1100

    rename endpoints

--- app/routes/length.js
+++ app/routes/length.js
@@ -2,5 +2,5 @@ import {json} from "remix";
 
 export const action = async ({request}) => {
     const requestText = await request.text()
-    return json({status: 'All Good from not parsing', length: requestText.length})
+    return json({length: requestText.length})
 }
--- app/routes/parse.js
+++ app/routes/parse.js
@@ -3,5 +3,5 @@ import {json} from "remix";
 export const action = async ({request}) => {
     const requestText = await request.text()
     const requestJson = JSON.parse(requestText)
-    return json({status: 'All Good from parsing', count: requestJson.data.length})
+    return json({count: requestJson.data.length})
 }
