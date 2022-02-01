8580cea0b6352ed67a9f9fabc5a9540625cda3c7
Leon Spencer <682772+Alazoral@users.noreply.github.com>
2022-02-01 12:34:56 +1100

    add 'ignore' counter-example

--- app/routes/crash.js
+++ app/routes/crash.js
@@ -1,16 +1,7 @@
 import {json} from "remix";
-import fs from 'fs/promises'
-import {join} from "path";
 
-export const loader = async () => {
-    const filename = join(__dirname, '..', '..', 'data', 'crash.json')
-    console.log('Opening filename', filename)
-    const loadedText = await fs.readFile(filename, 'utf8')
-    const requestJson = JSON.parse(loadedText)
-    return json({status: 'All Good from FS', count: requestJson.data.length})
-}
-export const action = async ({request, params}) => {
+export const action = async ({request}) => {
     const requestText = await request.text()
     const requestJson = JSON.parse(requestText)
-    return json({status: 'All Good from Body', count: requestJson.data.length})
+    return json({status: 'All Good from parsing', count: requestJson.data.length})
 }
